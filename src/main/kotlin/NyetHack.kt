eb1534bbc1155e412ad0dcaa9f4c8ba95ee811fc
ymkim <ymkim@theunify.co.kr>
2026-01-28 21:12:22 +0900

    Listing 8.13

--- src/main/kotlin/NyetHack.kt
+++ src/main/kotlin/NyetHack.kt
@@ -1,5 +1,8 @@
 fun main() {
-    narrate("A hero enters the town of Kronstadt. What is their name?")
+    narrate("A hero enters the town of Kronstadt. What is their name?", { message ->
+        // Prints the message in yellow
+        "\u001b[33;1m$message\u001b[0m"
+    })
     val heroName = readLine() ?: ""
 
     changeNarratorMood()
