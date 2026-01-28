8df7a9274f4d1b502ec6b39e5c54f6c928c4ca80
ymkim <ymkim@theunify.co.kr>
2026-01-28 19:18:23 +0900

    Listing 8.10

--- src/main/kotlin/Narrator.kt
+++ src/main/kotlin/Narrator.kt
@@ -1,10 +1,47 @@
-val narrationModifier = { message: String ->
-    val numExclamationPoints = 3
-    message.uppercase() + "!".repeat(numExclamationPoints)
-}
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+var narrationModifier: (String) -> String = { it }
 
 fun narrate(
     message: String
 ) {
     println(narrationModifier(message))
+}
+
+fun changeNarratorMood() {
+    val mood: String
+    val modifier: (String) -> String
+
+    when (Random.nextInt(1..4)) {
+        1 -> {
+            mood = "loud"
+            modifier = { message ->
+                val numExclamationPoints = 3
+                message.uppercase() + "!".repeat(numExclamationPoints)
+            }
+        }
+        2 -> {
+            mood = "tired"
+            modifier = { message ->
+                val numExclamationPoints = 3
+                message.lowercase().replace(" ", "... ")
+            }
+        }
+        3 -> {
+            mood = "unsure"
+            modifier = { message ->
+                "$message?"
+            }
+        }
+        else -> {
+            mood = "professional"
+            modifier = { message ->
+                "$message."
+            }
+        }
+    }
+
+    narrationModifier = modifier
+    narrate("The narrator begins to feel $mood")
 }
