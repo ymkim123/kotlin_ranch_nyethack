aaa9bd7beb3c01c7def8de82d384b86e04810fd9
ymkim <ymkim@theunify.co.kr>
2026-01-28 17:20:53 +0900

    Listing 8.6

--- src/main/kotlin/Narrator.kt
+++ src/main/kotlin/Narrator.kt
@@ -1,10 +1,10 @@
+val narrationModifier: (String) -> String = { message ->
+    val numExclamationPoints = 3
+    message.uppercase() + "!".repeat(numExclamationPoints)
+}
+
 fun narrate(
     message: String
 ) {
-    val narrationModifier: () -> String = {
-        val numExclamationPoints = 3
-        message.uppercase() + "!".repeat(numExclamationPoints)
-    }
-
-    println(narrationModifier())
+    println(narrationModifier(message))
 }
