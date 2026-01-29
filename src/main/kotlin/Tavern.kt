cdf5038563cc048e969a47f772b31a37fef82836
ymkim <ymkim@theunify.co.kr>
2026-01-29 19:12:12 +0900

    Listing 9.20

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -30,6 +30,10 @@ fun visitTavern() {
         println("Good evening, $patron - you're #${index + 1} in line")
         placeOrder(patron, "Dragon's Breath")
     }
+
+    menuData.forEachIndexed { index, data ->
+        println("$index : $data")
+    }
 }
 
 private fun placeOrder(patronName: String, menuItemName: String) {
