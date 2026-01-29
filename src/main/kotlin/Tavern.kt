3b7d5e54af112e2a6f3a45eab24d5727cc08365a
ymkim <ymkim@theunify.co.kr>
2026-01-29 11:08:41 +0900

    Listing 9.3

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -2,4 +2,8 @@ private const val TAVERN_MASTER = "Taernyl"
 private const val TAVERN_NAME = "$TAVERN_MASTER's Folly"
 
 fun visitTavern() {
+    narrate("$heroName enters $TAVERN_NAME")
+
+    val patrons: List<String> = listOf("Eli", "Mordoc", "Sophie")
+    println(patrons)
 }
