fd9808531329a6a4e388d5bd5a6958d205aa81d0
ymkim <ymkim@theunify.co.kr>
2026-01-29 14:15:18 +0900

    Listing 9.18

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -22,5 +22,11 @@ fun visitTavern() {
 
     patrons.forEachIndexed { index, patron ->
         println("Good evening, $patron - you're #${index + 1} in line")
+        placeOrder(patron, "Dragon's Breath")
     }
+}
+
+private fun placeOrder(patronName: String, menuItemName: String) {
+    narrate("$patronName speaks with $TAVERN_MASTER to place an order")
+    narrate("$TAVERN_MASTER hands $patronName a $menuItemName")
 }
