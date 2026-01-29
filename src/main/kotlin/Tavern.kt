4c07723aac089960ce76cedde8619d60e62858f3
ymkim <ymkim@theunify.co.kr>
2026-01-29 11:12:22 +0900

    Listing 9.6

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -4,6 +4,6 @@ private const val TAVERN_NAME = "$TAVERN_MASTER's Folly"
 fun visitTavern() {
     narrate("$heroName enters $TAVERN_NAME")
 
-    val patrons: List<String> = listOf("Eli", "Mordoc", "Sophie")
-    println(patrons)
+    val patrons = listOf("Eli", "Mordoc", "Sophie")
+    println(patrons[0])
 }
