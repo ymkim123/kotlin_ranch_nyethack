f600764be5e86fb0b48f130e4ef99347008c0b4b
ymkim <ymkim@theunify.co.kr>
2026-01-29 11:08:09 +0900

    Listing 9.2

--- src/main/kotlin/NyetHack.kt
+++ src/main/kotlin/NyetHack.kt
@@ -1,15 +1,26 @@
+var heroName: String = ""
+
 fun main() {
+    heroName = promptHeroName()
+    // changeNarratorMood()
+    narrate("$heroName, ${createTitle(heroName)}, heads to the town square")
+    visitTavern()
+}
+
+private fun promptHeroName(): String {
     narrate("A hero enters the town of Kronstadt. What is their name?") { message ->
         // Prints the message in yellow
         "\u001b[33;1m$message\u001b[0m"
     }
-    val heroName = readLine()
-    require(heroName != null && heroName.isNotEmpty()) {
+
+    /*val input = readLine()
+    require(input != null && input.isNotEmpty()) {
         "The hero must have a name."
     }
 
-    changeNarratorMood()
-    narrate("$heroName, ${createTitle(heroName)}, heads to the town square")
+    return input*/
+    println("Madrigal")
+    return "Madrigal"
 }
 
 private fun createTitle(name: String): String {
