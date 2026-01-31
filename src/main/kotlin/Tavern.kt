3bdce18df1f6c5773f4cabf3259dcedea93168ca
ymkim <ymkim@theunify.co.kr>
2026-01-31 18:01:59 +0900

    Listing 11.7

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -42,6 +42,9 @@ fun visitTavern() {
     narrate("$heroName sees several patrons in the tavern:")
     narrate(patrons.joinToString())
 
+    val favoriteItems = patrons.map { getFavoriteMenuItems(it)}
+    println("Favorite items: $favoriteItems")
+
     repeat(3) {
         placeOrder(patrons.random(), menuItems.random(), patronGold)
     }
