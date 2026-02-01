f11bd5523347fa2c8cdb226a9172c9b777d9bb3d
ymkim <ymkim@theunify.co.kr>
2026-02-01 09:21:59 +0900

    Listing 11.13

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -34,11 +34,9 @@ fun visitTavern() {
 
     val patronGold = mutableMapOf(
         TAVERN_MASTER to 86.00,
-        heroName to 4.50
+        heroName to 4.50,
+        *patrons.map { it to 6.00 }.toTypedArray()
     )
-    patrons.forEach { patronName ->
-        patronGold += patronName to 6.0
-    }
 
     narrate("$heroName sees several patrons in the tavern:")
     narrate(patrons.joinToString())
