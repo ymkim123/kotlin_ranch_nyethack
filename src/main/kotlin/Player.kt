e14c9ec64c149bf9cfcd701a2cd3185e2061047f
ymkim <theduke2001@naver.com>
2026-02-10 17:23:36 +0900

    Listing 13.8 * 실행 성공

--- src/main/kotlin/Player.kt
+++ src/main/kotlin/Player.kt
@@ -1,5 +1,6 @@
 class Player {
     val name = "madrigal"
+        get() = field.replaceFirstChar { it.uppercase() }
 
     fun castFireball(numFireballs:Int = 2) {
         narrate("A glass of Fireball springs into existence (x$numFireballs)")
