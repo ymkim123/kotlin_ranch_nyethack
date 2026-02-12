654ca0274f7bbb862d2307a586963eb5c2caf6f2
ymkim <theduke2001@naver.com>
2026-02-13 08:35:16 +0900

    Listing 13.13

--- src/main/kotlin/Player.kt
+++ src/main/kotlin/Player.kt
@@ -5,7 +5,20 @@ class Player {
             field = value.trim()
         }
 
+    val title: String
+        get() = when {
+            name.all { it.isDigit() } -> "The Identifiable"
+            name.none { it.isLetter() } -> "The Witness Protection Member"
+            name.count { it.lowercase() in "aeiou" } > 4 -> "The Master of Vowels"
+            else -> "The Renowned Hero"
+        }
+
     fun castFireball(numFireballs:Int = 2) {
         narrate("A glass of Fireball springs into existence (x$numFireballs)")
+    }
+
+    fun changeName(newName: String) {
+        narrate("$name legally changes their name to $newName")
+        name = newName
     }
 }
