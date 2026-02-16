6a8454076d2caf418c284e6a261cf07b021bbef7
ymkim <ymkim@theunify.co.kr>
2026-02-16 23:26:33 +0900

    Listing 14.14

--- src/main/kotlin/com/bignerdranch/nyethack/NyetHack.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/NyetHack.kt
@@ -8,12 +8,14 @@ fun main() {
     val playerName = promptHeroName()
     player = Player(playerName)
     // changeNarratorMood()
+    player.prophesize()
     val mortality = if (player.isImmortal) "an immortal" else "a mortal"
     narrate("${player.name} of ${player.hometown}, ${player.title}, heads to the town square")
     narrate("${player.name}, $mortality, has ${player.healthPoints} health points")
 
     visitTavern()
     player.castFireball()
+    player.prophesize()
 }
 
 private fun promptHeroName(): String {
