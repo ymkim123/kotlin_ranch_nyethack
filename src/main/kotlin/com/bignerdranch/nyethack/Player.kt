5eb51f99ddbc80985fdc79c9916249744ba086a5
ymkim <ymkim@theunify.co.kr>
2026-02-16 23:25:55 +0900

    Listing 14.13

--- src/main/kotlin/com/bignerdranch/nyethack/Player.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/Player.kt
@@ -20,6 +20,19 @@ class Player(
             else -> "The Renowned Hero"
         }
 
+    val prophecy by lazy {
+        narrate("$name embarks on an arduous quest to locate a fortune teller")
+        Thread.sleep(3000)
+        narrate("The fortune teller bestows a prophecy upon $name")
+
+        "An intrepid hero from $hometown shall some day " + listOf(
+            "form an unlikely bond between two warring factions",
+            "take possession of an otherworldly blade",
+            "bring the gift of creation back to the world",
+            "best the world-eater"
+        ).random()
+    }
+
     init {
         require(healthPoints > 0) { "healthPoints must be greater than zero" }
         require(name.isNotBlank()) { "Player must have a name" }
@@ -42,5 +55,10 @@ class Player(
     fun changeName(newName: String) {
         narrate("$name legally changes their name to $newName")
         name = newName
+    }
+
+    fun prophesize() {
+        narrate("$name thinks about their future")
+        narrate("A fortune teller told Madrigal, \"$prophecy\"")
     }
 }
