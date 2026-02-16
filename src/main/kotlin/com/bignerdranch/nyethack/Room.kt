b3e0fcaabc967cc82f748ed810f21bced77a0ccf
ymkim <ymkim@theunify.co.kr>
2026-02-17 08:42:34 +0900

    Listing 15.1

--- src/main/kotlin/com/bignerdranch/nyethack/Room.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/Room.kt
@@ -1,4 +1,10 @@
 package com.bignerdranch.nyethack
 
-class Room {
+class Room(val name: String) {
+
+    fun description() = name
+    fun enterRoom() {
+        narrate("There is nothing to do here")
+    }
+
 }
