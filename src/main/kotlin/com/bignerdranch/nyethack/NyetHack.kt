6afa0f66f5aa516bf827f7f0dfd9ede220f05619
ymkim <ymkim@theunify.co.kr>
2026-02-16 23:24:47 +0900

    Listing 14.11

--- src/main/kotlin/com/bignerdranch/nyethack/NyetHack.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/NyetHack.kt
@@ -4,7 +4,7 @@ var heroName: String = ""
 lateinit var player: Player
 
 fun main() {
-    narrate("Welcome to NyetHack!")
+    narrate("Welcome to NyetHack, ${player.name}!")
     val playerName = promptHeroName()
     player = Player(playerName)
     // changeNarratorMood()
