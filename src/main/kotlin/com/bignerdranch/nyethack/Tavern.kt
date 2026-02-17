807009c19b7950a2063229153c20cc4227f8f76e
ymkim <ymkim@theunify.co.kr>
2026-02-17 13:52:20 +0900

    Listing 15.21

--- src/main/kotlin/com/bignerdranch/nyethack/Tavern.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/Tavern.kt
@@ -91,74 +91,12 @@ class Tavern : Room(TAVERN_NAME) {
     }
 }
 
-fun visitTavern() {
-    narrate("${player.name} enters $TAVERN_NAME")
-    narrate("There are several items for sale:")
-    narrate(menuItems.joinToString())
-
-    val patrons: MutableSet<String> = firstNames.shuffled()
-        .zip(lastNames.shuffled()) { firstName, lastName -> "$firstName $lastName" }
-        .toMutableSet()
-
-    val patronGold = mutableMapOf(
-        TAVERN_MASTER to 86.00,
-        player.name to 4.50,
-        *patrons.map { it to 6.00 }.toTypedArray()
-    )
-
-    narrate("${player.name} sees several patrons in the tavern:")
-    narrate(patrons.joinToString())
-
-    val itemOfDay = patrons.flatMap { getFavoriteMenuItems(it) }.random()
-    narrate("The item of the day is the $itemOfDay")
-
-    repeat(3) {
-        placeOrder(patrons.random(), menuItems.random(), patronGold)
-    }
-    displayPatronBalances(patronGold)
-
-    patrons.filter { patron -> patronGold.getOrDefault(patron, 0.0) < 4.0 }
-        .also { departingPatrons ->
-            patrons -= departingPatrons
-            patronGold -= departingPatrons
-        }
-        .forEach { patron ->
-            narrate("${player.name} sees $patron departing the tavern")
-        }
-
-    narrate("There are still some patrons in the tavern")
-    narrate(patrons.joinToString())
-}
-
 private fun getFavoriteMenuItems(patron: String): List<String> {
     return when (patron) {
         "Alex Ironfoot" -> menuItems.filter { menuItem ->
             menuItemTypes[menuItem]?.contains("dessert") == true
         }
         else -> menuItems.shuffled().take(Random.nextInt(1..2))
-    }
-}
-
-private fun placeOrder(
-    patronName: String,
-    menuItemName: String,
-    patronGold: MutableMap<String, Double>
-) {
-    val itemPrice = menuItemPrices.getValue(menuItemName)
-
-    narrate("$patronName speaks with $TAVERN_MASTER to place an order")
-    if (itemPrice <= patronGold.getOrDefault(patronName, 0.0)) {
-        val action = when (menuItemTypes[menuItemName]) {
-            "shandy", "elixir" -> "pours"
-            "meal" -> "serves"
-            else -> "hands"
-        }
-        narrate("$TAVERN_MASTER $action $patronName a $menuItemName")
-        narrate("$patronName pays $TAVERN_MASTER $itemPrice gold")
-        patronGold[patronName] = patronGold.getValue(patronName) - itemPrice
-        patronGold[TAVERN_MASTER] = patronGold.getValue(TAVERN_MASTER) + itemPrice
-    } else {
-        narrate("$TAVERN_MASTER says \"You need more coin for a $menuItemName\"")
     }
 }
 
