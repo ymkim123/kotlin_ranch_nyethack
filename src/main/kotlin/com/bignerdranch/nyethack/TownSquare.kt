19e82e692cdfd2a4af0378050d28c707cf0cfa85
ymkim <ymkim@theunify.co.kr>
2026-02-17 09:45:53 +0900

    Listing 15.8

--- src/main/kotlin/com/bignerdranch/nyethack/TownSquare.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/TownSquare.kt
@@ -1,6 +1,8 @@
 package com.bignerdranch.nyethack
 
 class TownSquare : Room("THe Town Square") {
+    override val status = "Bustling"
+
     override fun enterRoom() {
         narrate("The villagers rally and cheer as the hero enters")
     }
