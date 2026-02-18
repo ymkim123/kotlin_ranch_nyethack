4082f85169f0f6a6a8d670bec691ebdc5285cb02
ymkim <ymkim@theunify.co.kr>
2026-02-18 23:15:12 +0900

    Listing 16.17

--- src/main/kotlin/com/bignerdranch/nyethack/NyetHack.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/NyetHack.kt
@@ -29,7 +29,13 @@ private fun promptHeroName(): String {
 }
 
 object Game {
-    private var currentRoom: Room = TownSquare()
+    private val worldMap = listOf(
+        listOf(TownSquare(), Tavern(), Room("Back Room")),
+        listOf(Room("A long Corridor"), Room("A Generic Room")),
+        listOf(Room("The Dungeon"))
+    )
+
+    private var currentRoom: Room = worldMap[0][0]
     private var currentPosition = Coordinate(0, 0)
 
     init {
