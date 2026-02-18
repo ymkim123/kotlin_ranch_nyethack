94ee8461de1b1cbecbc76ba8f3a9d0474b7a5999
ymkim <ymkim@theunify.co.kr>
2026-02-18 16:02:11 +0900

    Listing 16.13

--- src/main/kotlin/com/bignerdranch/nyethack/Navigation.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/Navigation.kt
@@ -2,9 +2,11 @@ package com.bignerdranch.nyethack
 
 data class Coordinate(val x: Int, val y: Int)
 
-enum class Direction {
-    North,
-    East,
-    South,
-    West
+enum class Direction(
+    private val directionCoordinate: Coordinate
+) {
+    North(Coordinate(0, -1)),
+    East(Coordinate(1, 0)),
+    South(Coordinate(0, 1)),
+    West(Coordinate(-1, 0))
 }
