30462aefa0dc57858e52d6a05fbcc079dc97722a
ymkim <ymkim@theunify.co.kr>
2026-02-22 15:22:27 +0900

    Listing 18.17

--- src/main/kotlin/com/bignerdranch/nyethack/Room.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/Room.kt
@@ -3,6 +3,7 @@ package com.bignerdranch.nyethack
 open class Room(val name: String) {
 
     protected open val status = "Calm"
+    open val lootBox: LootBox<Loot> = LootBox.random()
 
     open fun description() = "$name (Currently: $status)"
     open fun enterRoom() {
