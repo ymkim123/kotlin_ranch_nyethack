65ddbe95cfd202d5a02a597e3228c2d933d1af17
ymkim <ymkim@theunify.co.kr>
2026-02-22 15:20:07 +0900

    Listing 18.16

--- src/main/kotlin/com/bignerdranch/nyethack/Loot.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/Loot.kt
@@ -1,5 +1,8 @@
 package com.bignerdranch.nyethack
 
+import kotlin.random.Random
+import kotlin.random.nextInt
+
 class LootBox<out T : Loot>(val contents: T) {
     var isOpen = false
         private set
@@ -7,6 +10,19 @@ class LootBox<out T : Loot>(val contents: T) {
     fun takeLoot(): T? {
         return contents.takeIf { !isOpen }
             .also { isOpen = true }
+    }
+
+    companion object {
+        fun random(): LootBox<Loot> = LootBox(
+            contents = when (Random.nextInt(1..100)) {
+                in 1..5 -> Fez("fez of immaculate style", 150)
+                in 6..10 -> Fedora("fedora of knowledge", 125)
+                in 11..15 -> Fedora("stunning teal fedora", 75)
+                in 16..30 -> Fez("ordinary fez", 15)
+                in 31..50 -> Fedora("ordinary fedora", 10)
+                else -> Gemstones(Random.nextInt(50..100))
+            }
+        )
     }
 }
 
