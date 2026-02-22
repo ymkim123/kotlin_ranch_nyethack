72b77a806067422b8c45d97e7b2ae2b10f9bba81
ymkim <ymkim@theunify.co.kr>
2026-02-22 09:34:06 +0900

    Listing 18.2

--- src/main/kotlin/com/bignerdranch/nyethack/Loot.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/Loot.kt
@@ -1,3 +1,16 @@
 package com.bignerdranch.nyethack
 
 class LootBox<T>(var contents: T)
+
+class Fedora(
+    val name: String,
+    val value: Int
+)
+
+class Gemstones(
+    val value: Int
+)
+
+class Key(
+    val name: String
+)
